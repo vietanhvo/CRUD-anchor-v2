ddf5ca7ad55424ec81c42de2a40242a82f8f0d7d
Anh Viet <anhviet5121pc@gmail.com>
2022-07-15 18:05:03 +0700

    Program

--- programs/CRUD-anchor-v2/src/lib.rs
+++ programs/CRUD-anchor-v2/src/lib.rs
@@ -1,15 +1,45 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GD6s6jBdxFf7pbvxx87uPQmvNYKuVXGfiziJCVkFmE2F");
 
 #[program]
 pub mod crud_anchor_v2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 64)]
+    base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub data: String,
+    pub data_list: Vec<String>,
+}
